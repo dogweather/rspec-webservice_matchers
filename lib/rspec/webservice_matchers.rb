a08219b89bd56c99b8cd86443e36362f527d370d
Robb Shecter <robb@weblaws.org>
2014-01-07 15:12:11 -0800

    first commit

---
+++ lib/rspec/webservice_matchers.rb
@@ -0,0 +1,7 @@
+require "rspec/webservice_matchers/version"
+
+module Rspec
+  module WebserviceMatchers
+    # Your code goes here...
+  end
+end
---
+++ lib/rspec/webservice_matchers/version.rb
@@ -0,0 +1,5 @@
+module Rspec
+  module WebserviceMatchers
+    VERSION = "0.0.1"
+  end
+end
