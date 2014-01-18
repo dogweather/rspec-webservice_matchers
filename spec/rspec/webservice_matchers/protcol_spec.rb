663c244fa958334adb526a73c7fb7bf6c6445c0e
Robb Shecter <robb@weblaws.org>
2014-01-18 01:46:13 -0800

    Simplified some specs

--- spec/rspec/webservice_matchers/protcol_spec.rb
+++ spec/rspec/webservice_matchers/protcol_spec.rb
@@ -1,19 +1,18 @@
 require 'rspec/webservice_matchers'
 
+#
 # TODO: set up a test server or mocks for these.
 #
 describe 'status_code' do
-  describe 'passes when the server returns the expected value' do
-    it 'can check 200 for successful resource requests' do
-      'http://www.rfc-editor.org/rfc/rfc2616.txt'.should be_status 200
-    end
+  it 'can check 200 for successful resource requests' do
+    'http://www.rfc-editor.org/rfc/rfc2616.txt'.should be_status 200
+  end
 
-    it 'handles domain names in addition to URLs' do
-      'www.rfc-editor.org'.should be_status 200
-    end
+  it 'handles domain names as well as URLs' do
+    'www.rfc-editor.org'.should be_status 200
+  end
 
-    it 'can check 503 for the Service Unavailable status' do
-      'http://www.weblaws.org/texas/laws/tex._spec._dist._local_laws_code_section_1011.202_tax_to_pay_general_obligation_bonds'.should be_status 503
-    end
+  it 'can check 503 for the Service Unavailable status' do
+    'http://www.weblaws.org/texas/laws/tex._spec._dist._local_laws_code_section_1011.202_tax_to_pay_general_obligation_bonds'.should be_status 503
   end
 end
