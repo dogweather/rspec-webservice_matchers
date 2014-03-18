9e4d783b13ec8a2c4b1c5d12fe0730c8bacb8084
Robb Shecter <robb@weblaws.org>
2014-03-18 01:31:20 -0700

    first failing spec for temp redirect

--- spec/rspec/webservice_matchers/redirect_spec.rb
+++ spec/rspec/webservice_matchers/redirect_spec.rb
@@ -4,38 +4,38 @@ require 'rspec/webservice_matchers'
 
 describe 'redirect_permanently_to' do
   it 'passes when receiving a 301 to the given URL' do
-    expect('http://perm-redirector.com').to redirect_permanently_to('http://www.website.com/')
+    expect('http://perm-redirector.com').to redirect_permanently_to 'http://www.website.com/' 
   end
 
   it 'handles domain names gracefully' do
-    expect('perm-redirector.com').to redirect_permanently_to('www.website.com/')
+    expect('perm-redirector.com').to redirect_permanently_to 'www.website.com/'
   end
 
   it 'handles a missing final slash' do
-    expect('perm-redirector.com').to redirect_permanently_to('www.website.com')
+    expect('perm-redirector.com').to redirect_permanently_to 'www.website.com'
   end 
 
   it 'gives a good error message for the wrong redirect type' do
     expect {
-      expect('temp-redirector.org').to redirect_permanently_to('http://a-page.com/a/page.txt')
+      expect('temp-redirector.org').to redirect_permanently_to 'http://a-page.com/a/page.txt'
     }.to fail_matching(/temporary/i)
   end
 
   it 'gives a good error message for a redirect to the wrong location' do
     expect {
-      expect('perm-redirector.com').to redirect_permanently_to('http://the-wrong-site.com/')
+      expect('perm-redirector.com').to redirect_permanently_to 'http://the-wrong-site.com/'
     }.to fail_matching(/location/i)
   end
 
   it 'gives a good error message for a non-redirect status' do
     expect {
-      expect('notfound.com').to redirect_permanently_to('http://the-wrong-site.com/')
+      expect('notfound.com').to redirect_permanently_to 'http://the-wrong-site.com/'
     }.to fail_matching(/404/i)    
   end
 
   it 'gives a good error message when the hostname is bad' do
     expect {
-      expect('asdhfjadhsfksd.com').to redirect_permanently_to('http://the-wrong-site.com/')
+      expect('asdhfjadhsfksd.com').to redirect_permanently_to 'http://the-wrong-site.com/'
     }.to fail_matching(/not known/i)    
   end    
 end
@@ -54,7 +54,12 @@ describe 'redirect_temporarily_to' do
     'temp-307-redirector.net'.should redirect_temporarily_to 'a-page.com/a/page.txt'
   end
 
-  it 'gives a good error message for the wrong redirect type'
+  it 'gives a good error message for the wrong redirect type' do
+    expect {
+      expect('perm-redirector.com').to redirect_temporarily_to 'www.website.com/'
+    }.to fail_matching(/permanent/i)
+  end
+
   it 'gives a good error message for a redirect to the wrong location'
   it 'gives a good error message for a non-redirect status'
   it 'gives a good error message when the hostname is bad'
