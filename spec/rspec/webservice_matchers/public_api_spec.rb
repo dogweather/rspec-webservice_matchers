2c8dfd47d6066d3b9a5784ee12cdb3bce7aec447
Taylor Fausak <taylor@fausak.me>
2015-01-20 20:16:25 -0600

    Fix RSpec 2.99 warnings

--- spec/rspec/webservice_matchers/public_api_spec.rb
+++ spec/rspec/webservice_matchers/public_api_spec.rb
@@ -3,11 +3,11 @@ require 'rspec/webservice_matchers'
 
 describe '#up?' do
   it 'follows redirects when necessary' do
-    RSpec::WebserviceMatchers.up?('perm-redirector.com').should be_true
-    RSpec::WebserviceMatchers.up?('temp-redirector.org').should be_true
+    RSpec::WebserviceMatchers.up?('perm-redirector.com').should be_truthy
+    RSpec::WebserviceMatchers.up?('temp-redirector.org').should be_truthy
   end
 
   it 'retries timeout errors once' do
-    RSpec::WebserviceMatchers.up?('http://www.timeout-once.com').should be_true
+    RSpec::WebserviceMatchers.up?('http://www.timeout-once.com').should be_truthy
   end
 end
