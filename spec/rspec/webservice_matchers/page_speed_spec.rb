e576f36c8fcc99958a56b832dc76ea6a06acf5c7
Robb Shecter <robb@weblaws.org>
2016-02-25 23:09:56 -0800

    Failing spec and fixture

--- spec/rspec/webservice_matchers/page_speed_spec.rb
+++ spec/rspec/webservice_matchers/page_speed_spec.rb
@@ -4,8 +4,20 @@ require 'spec_helper'
 require 'rspec/webservice_matchers'
 require 'rspec/webservice_matchers/util'
 
+SAMPLE_JSON_RESPONSE = 'spec/fixtures/pagespeed.json'
+
 describe 'be_fast' do
   it 'is in a module which is loaded' do
     expect(RSpec::WebserviceMatchers::BeFast).not_to be_nil
+  end
+end
+
+describe RSpec::WebserviceMatchers::BeFast do
+  describe '#parse' do
+    it 'can parse the overall score' do
+      api_response = File.read(SAMPLE_JSON_RESPONSE)
+      data = RSpec::WebserviceMatchers::BeFast.parse(json: api_response)
+      expect(data[:score]).to eq(85)
+    end
   end
 end
