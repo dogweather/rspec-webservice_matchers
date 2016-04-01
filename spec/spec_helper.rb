b9a033ebe9254437dd5ce9dfcda79aa38c13fc72
Robb Shecter <robb@weblaws.org>
2016-03-31 20:39:47 -0700

    Cleans up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,47 +4,50 @@ require 'webmock/rspec'
 
 RSpec.configure do |config|
   config.before(:each) do
-  # HOSTS WHICH RETURN ERRORS
-  WebMock.stub_request(:any, /notfound.com/).to_return(status: 404)
-  WebMock.stub_request(:any, 'outoforder.com').to_return(status: 503)
-  WebMock.stub_request(:any, 'not-a-domain.com')
-         .to_raise(Faraday::ConnectionFailed.new('Failed to open TCP connection to asdhfjkahsdfadfd.com:80 (getaddrinfo: nodename nor servname provided, or not known)'))
+    # HOSTS WHICH RETURN ERRORS
+    WebMock.stub_request(:any, /notfound.com/).to_return(status: 404)
+    WebMock.stub_request(:any, 'outoforder.com').to_return(status: 503)
+    WebMock.stub_request(:any, 'not-a-domain.com')
+           .to_raise(Faraday::ConnectionFailed.new('Failed to open TCP connection to asdhfjkahsdfadfd.com:80 (getaddrinfo: nodename nor servname provided, or not known)'))
 
+    # FUNCTIONING WEB PAGES
+    WebMock.stub_request :any, 'http://a-page.com/a/page.txt'
+    WebMock.stub_request :any, 'www.website.com'
 
-  WebMock.stub_request :any, 'http://a-page.com/a/page.txt'
-  WebMock.stub_request :any, 'www.website.com'
+    # A HOST WHICH DOESN'T SUPPORT HEAD
+    WebMock.stub_request(:head, 'appengine.com').to_return(status: 405)
+    WebMock.stub_request(:get,  'appengine.com').to_return(status: 200)
 
-  # A host which doesn't support HEAD
-  WebMock.stub_request(:head, 'appengine.com').to_return(status: 405)
-  WebMock.stub_request(:get,  'appengine.com').to_return(status: 200)
+    # FUNCTIONING REDIRECTS
+    WebMock.stub_request(:head, 'http://perm-redirector.com/')
+           .to_return(status: 301,
+                      body: '',
+                      headers: { Location: 'http://www.website.com/' })
 
-  # WORKING REDIRECTS
-  WebMock.stub_request(:head, 'http://perm-redirector.com/')
-         .to_return(status: 301,
-                    body: '',
-                    headers: { Location: 'http://www.website.com/' })
+    WebMock.stub_request(:any, 'temp-redirector.org')
+           .to_return(status: 302,
+                      headers: { Location: 'http://a-page.com/a/page.txt' })
 
-  WebMock.stub_request(:any, 'temp-redirector.org')
-         .to_return(status: 302, headers: { Location: 'http://a-page.com/a/page.txt' })
+    WebMock.stub_request(:any, 'temp-307-redirector.net')
+           .to_return(status: 307,
+                      headers: { Location: 'http://a-page.com/a/page.txt' })
 
-  WebMock.stub_request(:any, 'temp-307-redirector.net')
-         .to_return(status: 307, headers: { Location: 'http://a-page.com/a/page.txt' })
+    # TIMEOUT SCENARIOS
+    WebMock.stub_request(:any, 'www.timeout.com').to_timeout
+    WebMock.stub_request(:any, 'www.timeout-once.com').to_timeout
+           .then.to_return(body: 'abc')
 
-  # Timeout scenarios
-  WebMock.stub_request(:any, 'www.timeout.com').to_timeout
-  WebMock.stub_request(:any, 'www.timeout-once.com').to_timeout.then.to_return(body: 'abc')
+    # Insights API
+    key = ENV['WEBSERVICE_MATCHER_INSIGHTS_KEY']
+    WebMock.stub_request(:get, "https://www.googleapis.com/pagespeedonline/v2/runPagespeed?key=#{key}&screenshot=false&url=http://nonstop.qa")
+           .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent' => 'Faraday v0.9.2' })
+           .to_return(
+             status: 200,
+             body: IO.read('spec/fixtures/pagespeed.json'),
+             headers: {})
 
-  # Insights API
-  key = ENV['WEBSERVICE_MATCHER_INSIGHTS_KEY']
-  WebMock.stub_request(:get, "https://www.googleapis.com/pagespeedonline/v2/runPagespeed?key=#{key}&screenshot=false&url=http://nonstop.qa")
-         .with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent' => 'Faraday v0.9.2' })
-         .to_return(
-           status: 200,
-           body: IO.read('spec/fixtures/pagespeed.json'),
-           headers: {})
-
-  # WebMock.allow_net_connect!
-end
+    # WebMock.allow_net_connect!
+  end
 end
 
 module RSpec
